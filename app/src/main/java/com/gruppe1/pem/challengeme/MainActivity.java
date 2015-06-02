398963cc9df0d78b174f948d206c6055f22db9a9
Alllexa <a-ronge@gmx.de>
2015-06-02 21:39:44 +0200

    implemented navigation tabs with ACTIVITIES (yess ;) )

--- app/src/main/java/com/gruppe1/pem/challengeme/MainActivity.java
+++ app/src/main/java/com/gruppe1/pem/challengeme/MainActivity.java
@@ -1,5 +1,9 @@
 package com.gruppe1.pem.challengeme;
 
+import android.app.LocalActivityManager;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,17 +11,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ActionBarDrawerToggle mDrawerToggle;
     DrawerLayout mDrawerLayout;
-
+    LocalActivityManager mlam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mlam = new LocalActivityManager(this, false);
+        mlam.dispatchCreate(savedInstanceState);
+        TabHost tabs=(TabHost)findViewById(R.id.tabHost);
+
+        tabs.setup(mlam);
+
+        TabHost.TabSpec spec=tabs.newTabSpec("tag1");
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+        spec.setContent(intent);
+        spec.setIndicator("Categories");
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Compare");
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("tag3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Wishlist");
+        tabs.addTab(spec);
+
+        for(int i=0;i<tabs.getTabWidget().getChildCount();i++)
+        {
+            TextView tv = (TextView) tabs.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tv.setTextColor(Color.parseColor("#FFFFFF"));
+        }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -36,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+
     }
 
     @Override
@@ -60,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -67,5 +103,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         mDrawerToggle.syncState();
+    }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mlam.dispatchResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mlam.dispatchPause(isFinishing());
     }
 }
